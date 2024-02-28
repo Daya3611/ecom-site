74b28f64de741e6715436d3052b96bca78046e78
r2hu1 <newasram86@gmail.com>
2024-02-28 21:57:15 +0530

    fixed icons

--- src/Cpmponets/Breadcrums/Breadcrums.jsx
+++ src/Cpmponets/Breadcrums/Breadcrums.jsx
@@ -3,12 +3,16 @@ import './Breadcrums.css'
 import arro_icon from '../assets/breadcrum_arrow.png'
 
 const Breadcrums = (props) => {
-    const {product} = props;
+  const { product } = props;
 
   return (
     <div className='breadcrums'>
-        Home <img src={arro_icon} alt="" /> Shop <img src={arro_icon} alt="" /> 
-        {product ? `${product.category} <img src={arro_icon} alt="" /> ${product.name}` : 'Loading...'}
+      Home <img src={arro_icon} alt="" /> Shop <img src={arro_icon} alt="" />
+      {product ? (
+        <>
+          {product.category} <img src={arro_icon} alt="" /> {product.name}
+        </>
+      ) : 'Loading...'}
     </div>
   )
 }
